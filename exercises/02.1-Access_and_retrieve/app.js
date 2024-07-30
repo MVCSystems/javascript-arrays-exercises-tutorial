6bcc17e5e9c9ea2dfe7f1776fcdeea18a7982514
MVCSystems <carlosmerav@gmail.com>
2024-07-30 15:38:13 -0500

    Add

--- exercises/02.1-Access_and_retrieve/app.js
+++ exercises/02.1-Access_and_retrieve/app.js
@@ -2,7 +2,9 @@
 let myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
 
 // 1. print the 3rd item here
-
 // 2. change the 'thursday' value to null here
-
 // 3. print the position of step 2
+
+console.log(myArray[2]);
+myArray[4] = null;
+console.log(myArray.indexOf(null));
