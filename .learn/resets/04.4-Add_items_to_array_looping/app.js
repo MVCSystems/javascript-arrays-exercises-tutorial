dac376685286791315bbfd5fea0513e09eebe9b5
MVCSystems <carlosmerav@gmail.com>
2024-07-30 16:05:03 -0500

    Add

--- .learn/resets/04.4-Add_items_to_array_looping/app.js
+++ .learn/resets/04.4-Add_items_to_array_looping/app.js
@@ -3,6 +3,7 @@ let arr = [4,5,734,43,45];
 //*****************
 // Your code here
 // 1: Agrega 10 números enteros aleatorios a la lista arr e imprime el array en la consola.
+// you need to loop 10 times, for example, using a for loop
 
 arr.push(Math.floor(Math.random()*100), Math.floor(Math.random()*100));
 console.log(arr);
--- exercises/04.3-Add_items_to_array/app.js
+++ exercises/04.3-Add_items_to_array/app.js
@@ -1,5 +1,9 @@
+// 1: Agrega 2 números enteros aleatorios a la lista arr (no necesitas un loop o bucle) e imprime el array en la consola.
+
 let arr = [4,5,734,43,45];
-
 // Your code here, use the push() function and the 2 Math functions
+// arr.push(Math.floor(Math.random()*100));
+// arr.push(Math.floor(Math.random()*100));
 
+arr.push(Math.floor(Math.random()*100), Math.floor(Math.random()*100));
 console.log(arr);
