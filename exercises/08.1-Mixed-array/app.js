961007bd861d64e0a7553d9f9d05384135a99643
MVCSystems <carlosmerav@gmail.com>
2024-07-30 17:52:03 -0500

    Add

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,12 @@
 let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 // Your code here
+// Usando un bucle, imprime un nuevo array en la consola que contenga los tipos de valores (data-types) que contiene el array mix en cada posición.
+
+let types = [];
+
+for (let i = 0; i < mix.length; i++) {
+    types.push(typeof mix[i]);
+    }
+
+console.log(types); // ["number", "boolean", "string", "object", "string", "number", "object"]
--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -1,6 +1,12 @@
 function sumTheElements(theArray) {
 	let total = 0;
 	// Your code here
+	// Usando un bucle (loop) for, completa el código de la función sumTheElements para que devuelva la suma de todos los elementos en un array dado.
+	// Si el argumento es [2, 13, 34, 5], la función debería devolver 54.
 
+	for (let i = 0; i < theArray.length; i++) {
+		total += theArray[i];
+	}
 	return total;
 }
+console.log(sumTheElements([2, 13, 34, 5]));
